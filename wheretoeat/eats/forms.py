99b739b14f3ecb5b33d3e4e308cb539cd52a6ce3
Adam McKerlie <adammckerlie@gmail.com>
2010-09-25 15:46:38 -0400

    Fixed float

--- wheretoeat/eats/forms.py
+++ wheretoeat/eats/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class QueryForm(forms.Form):
-    q = forms.CharField(widget=forms.TextInput(attrs={'class':'small-input'}))
+    q = forms.CharField(widget=forms.TextInput(attrs={'class':'small-input'}), initial='What should I eat?')
     postal_code = forms.CharField(widget=forms.TextInput(attrs={'class':'small-postal'}), initial='M5V 2H5')
