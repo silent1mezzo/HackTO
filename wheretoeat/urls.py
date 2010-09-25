4b4c3bc3ada32676fe02654112db6c6f9638e664
Ash Christopher <ash.christopher@gmail.com>
2010-09-25 04:49:09 -0400

    Added site-media

--- wheretoeat/urls.py
+++ wheretoeat/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    url(r'^site-media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root' : settings.MEDIA_ROOT }, name='media'),
 )
