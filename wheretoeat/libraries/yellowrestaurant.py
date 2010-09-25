5d77c3ec47af0be77cffebb81abe0249eb66b4a2
Terence Lo <terence.lo@gmail.com>
2010-09-25 11:06:30 -0500

    remove unnecessary keys.

--- wheretoeat/libraries/yellowrestaurant.py
+++ wheretoeat/libraries/yellowrestaurant.py
@@ -1,5 +1,6 @@
 from yellowapi import *
 import json
+import random
 
 class YellowRestaurantAPI(YellowAPI):
     def __init__(self, api_key, test_mode=False, format='JSON', handlers=[]):
@@ -11,7 +12,19 @@ class YellowRestaurantAPI(YellowAPI):
         resultsDict = json.loads(self.find_business(what,where,uid,page,page_len, sflag, lang))
         listings = resultsDict.get('listings')
         filteredResults = []
+        
         for listing in listings:
             if float(listing.get('distance')) < float(maxDistance):
+                self.clean_listing(listing)
                 filteredResults.append(listing)
-        return filteredResults 
+
+        return filteredResults
+    
+    # given yp listing result, remove unneeded keys
+    def clean_listing(self,listing):
+        keys = ['isParent', 'content', 'parentId']
+        for key in keys:
+            if listing.has_key(key):
+                del listing[key]
+        return listing
+
