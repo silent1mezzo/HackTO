840cfb8938903466ce11f585e4e44e4ac6e0d52e
Adam McKerlie <adammckerlie@gmail.com>
2010-09-25 11:09:17 -0400

    Change

--- wheretoeat/urls.py
+++ wheretoeat/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^', include('eats.urls')),
 )
